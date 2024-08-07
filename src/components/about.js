c9346e0f2402221d2cfd148e2e4354f334c46d97
Sukhjit Singh <sukhjitsingh@Sukhjits-MacBook-Air.local>
2024-08-07 15:52:12 -0400

    changes in about

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,7 @@ import react from 'react';
 function About(){
     return(
         <div style={{color:"white",textAlign:"center",width: "750px", margin: "10px auto",marginBottom:"400px",marginTop:"200px"}}>About Me
-            <p style={{fontSize:"20px",color:"white",}}>I'm full stack web developer. I completed my certification as a web developer from Bootcamp Columbia. I have bachelors in criminal Justice Management from John Jay. I am also a part time Real Estate sales Person. I am very passonate about designing new websites. I am excellent self learner. I am very punctuate.</p>
+            <p style={{fontSize:"20px",color:"white",}}>I’m a full-stack web developer with a certification from Bootcamp Columbia, and I hold a bachelor's degree in Criminal Justice Management from John Jay. In addition to my technical skills, I work part-time as a real estate salesperson. I’m deeply passionate about web design and thrive on creating innovative and engaging websites. My ability to self-learn and my punctual nature are key strengths that drive my success in all my endeavors.</p>
         </div>
     )
 }
